44d9200fd7889338e7b176606ccfdcd5d0e41b7b
ZasticBradyn <Zastixxoncrack@gmail.com>
2022-06-22 07:50:57 -0500

    'madev2'

--- blooket-utility/setup.js
+++
@@ -1,7 +0,0 @@
-if (!blooketUtility.prefs.blooketUI) {
-    blooketUtility.prefs.blooketUI = {
-      title: "BlooketUI",
-      description: "A script that implements a GUI to make cheating on Blooket easier than ever.",
-      value: false,
-    };
-  }
