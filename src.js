3cff4751f9d71f06c1d19c335bdaf601d916e1ec
ZasticBradyn <Zastixxoncrack@gmail.com>
2022-06-22 22:48:17 -0500

    'bunchOfThings'

--- src.js
+++ src.js
@@ -1,4 +1,6 @@
-let loop;
+// Currently fully coded by zastix
+
+let onlyloop;
 
 // Iframe bypass
 frame = document.createElement('iframe')
@@ -24,6 +26,31 @@ maingui.style.color = 'white'
 maingui.style.top = '5%'
 maingui.style.left = '40%'
 maingui.style.overflow = 'hidden'
+
+/// Make Main Div Draggable
+var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+GUI.onmousedown = ((e = window.event) => {
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = (() => {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    });
+    document.onmousemove = ((e) => {
+        e = e || window.event;
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        GUI.style.top = (GUI.offsetTop - pos2) + "px";
+        GUI.style.left = (GUI.offsetLeft - pos1) + "px";
+    });
+});
 
 // CSS styling
 style = document.createElement('style')
@@ -76,10 +103,10 @@ closeBtn.style.paddingTop = '10px'
 closeBtn.style.paddingRight = '15px'
 closeBtn.style.color = 'white'
 closeBtn.innerText = 'X'
-        closeBtn.onclick = () => {
-            GUI.remove();
-            clearInterval(loop);
-        }
+closeBtn.onclick = () => {
+    GUI.remove();
+    clearInterval(onlyloop);
+}
 
 // Minus Button
 minusBtn = document.createElement('button')
@@ -105,39 +132,71 @@ bodyDiv = document.createElement('div')
 bodyDiv.style.display = 'block'
 bodyDiv.style.margin = '10px'
 bodyDiv.style.minHeight = '70px'
-body.innerHTML = (`<span id="curPageEl">${getSite(true) ? `current gamemode: ${getSite(true)}` : 'no game detected'}</span><br><span>(press e to hide)</span><br>`);
+body.innerHTML = (`<span id="curPageEl">no game detected</span><br><span>(press e to hide)</span><br>`);
 
+// Global Div
+gbal = document.createElement('details');
+gbal.innerHTML = (`<summary style="padding: 10px; font-size: 1.5em; font-weight: bolder">Global</summary>`);
 
+// Cheat Div
+cheatDiv = document.createElement('div');
 
+// Footer Div
+footer = document.createElement('div');
+footer.style.fontSize = '0.9rem';
+footer.style.paddingBottom = '5px';
+footer.innerHTML = (`Open source on <a href="https://github.com/Blooketware/BlooketUI">GitHub</a><br>V2 Coded by <a href='https://github.com/notzastix'>zastix</a>`);
 
+// React Event Handler
+function reactEventHandler() {
+    return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner;
+}
 
-    let element = document.createElement('div');
-    element.innerHTML = `<div id="GUI"> <style>details > summary{cursor: pointer; transition: 1s; list-style: circle;}.hack{border: none; background: hsl(0, 0%, 20%); padding: 7px; margin: 5px; width: 70%; color: white; transition: 0.1s; border-radius: 5px; cursor: pointer;}.hack:hover{background: hsl(0, 1%, 31%);}</style> <div style="cursor: all-scroll; padding-top: 2px; font-size: 1.5rem; text-align: center;">BlooketUI<button id="gui-" style="background: black; height: 45px; width: 45px; border: none; cursor: pointer; position: absolute; top: -10px; right: 90%; font-size: 2.5rem; border-radius: 10px; font-family: Nunito; font-weight: bolder; padding-top: -10px; padding-right: -15px; color: white;">-</button> <button id="guiX" style="background: black; height: 45px; width: 45px; border: none; cursor: pointer; position: absolute; top: -10px; right: -10px; font-size: 1.5rem; border-radius: 10px; font-family: Nunito; font-weight: bolder; padding-top: 10px; padding-right: 15px; color: white;">X</button> </div><div style="display: block; margin: 10px; min-height: 70px;"> <div id="curPage">no game detected</div><div id="name">name: null</div><div>(press e to hide)</div><details open=""> <summary style="padding: 10px; font-size: 1.5em; font-weight: bolder">Main</summary> <button id="token" class="hack">Get Daily Rewards</button> <button id="spoof" class="hack">Spoof Blooks</button> <button id="open" class="hack">Spam Open Boxes</button> <button id="sell" class="hack">Auto Sell Dupes</button> <button id="correct" class="hack">Every Answer Correct</button> </details><br><div id="LoadedGame"> </div><div> Open source on <a href="https://github.com/Blooketware/BlooketUI">GitHub</a></div></div>`;
-    element.style = `width: 350px; background: rgb(64, 64, 64); border-radius: 8px; position: absolute; text-align: center; font-family: Nunito; color: white; overflow: hidden; top: 5%; left: 40%;`;
-    document.body.appendChild(element);
-    var pos1 = 0,
-        pos2 = 0,
-        pos3 = 0,
-        pos4 = 0;
-    element.onmousedown = ((e = window.event) => {
-        e.preventDefault();
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = (() => {
-            document.onmouseup = null;
-            document.onmousemove = null;
-        });
+// Firebase Handler
+function firebaseHandler() {
+    return reactEventHandler().memoizedProps.firebase
+}
 
-        document.onmousemove = ((e) => {
-            e = e || window.event;
-            e.preventDefault();
-            pos1 = pos3 - e.clientX;
-            pos2 = pos4 - e.clientY;
-            pos3 = e.clientX;
-            pos4 = e.clientY;
-            let top = (element.offsetTop - pos2) > 0 ? (element.offsetTop - pos2) : 0;
-            let left = (element.offsetLeft - pos1) > 0 ? (element.offsetLeft - pos1) : 0;
-            element.style.top = top + "px";
-            element.style.left = left + "px";
-        });
-    });
+// Cheats
+cheats = {
+    rush: {
+        'Set Defense': async () => {
+            firebaseHandler().setVal({
+                id: reactEventHandler().stateNode.props.client.hostId,
+                path: 'c/' + reactEventHandler().stateNode.props.client.name + '/d',
+                val: Number(prompt('How much defense do you want?'))
+            });
+        },
+        'Set Blooks': async () => {
+            firebaseHandler().setVal({
+                id: reactEventHandler().stateNode.props.client.hostId,
+                path: 'c/' + reactEventHandler().stateNode.props.client.name + '/bs',
+                val: Number(prompt('How many blooks do you want?'))
+            });
+        },
+        'Reset Player\'s Blooks ': async () => {
+            e = Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner;
+            wait = ms => new Promise(r => setTimeout(r, ms));
+            target = prompt('Who would you like to reset?')
+            oldbs = e.stateNode.state.numBlooks
+            firebaseHandler().getDatabaseVal(e.stateNode.props.client.hostId, "c", async (players) => {
+                if (Object.keys(players).includes(target)) {
+                    target2 = players[target]
+                    for (i = 0; i < (target2.bs + target2.d + 4); i++) {
+                        await wait(1500)
+                        firebaseHandler().setVal({
+                            id: e.stateNode.props.client.hostId,
+                            path: 'c/' + e.stateNode.props.client.name + '/tat',
+                            val: target
+                        });
+                        firebaseHandler().setVal({
+                            id: e.stateNode.props.client.hostId,
+                            path: 'c/' + e.stateNode.props.client.name + '/bs',
+                            val: oldbs
+                        });
+                    }
+                }
+            })
+        }
+    }
+}
