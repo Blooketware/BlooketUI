cff9ce40df9e87dbdf65343a7d3b300e92fb8c60
somthing nive <zastixxoncrack@gmail.com>
2022-09-06 18:41:09 -0500

    fixed user api link

--- src.js
+++ src.js
@@ -74,7 +74,7 @@ function loadGUI() {
 }
 start();
 async function debuggerHelp(how) {
-    const response = await fetch('https://api.blooket.com/api/users/verify-token', {
+    const response = await fetch('https://api.blooket.com/api/users/verify-session', {
         method: "GET",
         headers: {
             "accept": "application/json, text/plain, */*",
@@ -412,7 +412,7 @@ async function getstat() {
 }
 async function handleData(type) {
     if (type = "elements") {
-        const response = await fetch('https://api.blooket.com/api/users/verify-token', {
+        const response = await fetch('https://api.blooket.com/api/users/verify-session', {
             method: "GET",
             headers: {
                 "accept": "application/json, text/plain, */*",
